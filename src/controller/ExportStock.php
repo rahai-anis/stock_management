b3b7b77cb3a4c2293825ddb57f58d5f1e1ec203c
anis <anismohamedrahai@gmail.com>
2024-07-02 10:49:14 +0100

    adding the popup on the navbar twig and creating exportstock controller with routes

---
+++ src/controller/ExportStock.php
@@ -0,0 +1,13 @@
+<?php
+namespace Anis\Stockmanagement\Controller;
+
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class ExportStock extends FrameworkBundleAdminController
+{
+    public function export_stock(){
+        dump('hello');die;
+    }
+}
